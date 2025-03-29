e72f9df4895545aa6cbcb94b44f6befa28ca714b
Sanjay <attellisanjay29@gmail.com>
2025-03-29 16:38:20 +0530

    Initial commit

--- app.py
+++ app.py
@@ -436,5 +436,5 @@ if not os.path.exists('templates/error.html'):
         f.write(error_html)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 9000))
     app.run(host='0.0.0.0', port=port, debug=True)
